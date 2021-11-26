8f45618ece91589354db143938e928096268dab1
unknown <yisehakmitiku@gmail.com>
2021-11-26 16:38:20 +0800

    Refactor calculator component with hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,49 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Display from './Display';
 import calculate from '../logic/calculate';
 import './calculator.css';
 
-class Calculator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      total: '0',
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [total, setTotal] = useState(0);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const handleClick = (val) => {
+    const res = calculate({ total, next, operation }, val);
+    setTotal(res.total);
+    setNext(res.next);
+    setOperation(res.operation);
+  };
 
-  handleClick = (val) => {
-    this.setState((prevState) => calculate(prevState, val));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <div className="calculator-grid">
-        <Display css="output" total={total} next={next} operation={operation} />
-        <Button buttonName="AC" handleClick={this.handleClick} />
-        <Button buttonName="+/-" handleClick={this.handleClick} />
-        <Button buttonName="%" handleClick={this.handleClick} />
-        <Button buttonName="÷" css="bg-secondary" handleClick={this.handleClick} />
-        <Button buttonName="7" handleClick={this.handleClick} />
-        <Button buttonName="8" handleClick={this.handleClick} />
-        <Button buttonName="9" handleClick={this.handleClick} />
-        <Button buttonName="x" css="bg-secondary" handleClick={this.handleClick} />
-        <Button buttonName="4" handleClick={this.handleClick} />
-        <Button buttonName="5" handleClick={this.handleClick} />
-        <Button buttonName="6" handleClick={this.handleClick} />
-        <Button buttonName="-" css="bg-secondary" handleClick={this.handleClick} />
-        <Button buttonName="1" handleClick={this.handleClick} />
-        <Button buttonName="2" handleClick={this.handleClick} />
-        <Button buttonName="3" handleClick={this.handleClick} />
-        <Button buttonName="+" css="bg-secondary" handleClick={this.handleClick} />
-        <Button buttonName="0" css="span-two" handleClick={this.handleClick} />
-        <Button buttonName="." handleClick={this.handleClick} />
-        <Button buttonName="=" css="bg-secondary" handleClick={this.handleClick} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calculator-grid">
+      <Display css="output" total={total} next={next} operation={operation} />
+      <Button buttonName="AC" handleClick={handleClick} />
+      <Button buttonName="+/-" handleClick={handleClick} />
+      <Button buttonName="%" handleClick={handleClick} />
+      <Button buttonName="÷" css="bg-secondary" handleClick={handleClick} />
+      <Button buttonName="7" handleClick={handleClick} />
+      <Button buttonName="8" handleClick={handleClick} />
+      <Button buttonName="9" handleClick={handleClick} />
+      <Button buttonName="x" css="bg-secondary" handleClick={handleClick} />
+      <Button buttonName="4" handleClick={handleClick} />
+      <Button buttonName="5" handleClick={handleClick} />
+      <Button buttonName="6" handleClick={handleClick} />
+      <Button buttonName="-" css="bg-secondary" handleClick={handleClick} />
+      <Button buttonName="1" handleClick={handleClick} />
+      <Button buttonName="2" handleClick={handleClick} />
+      <Button buttonName="3" handleClick={handleClick} />
+      <Button buttonName="+" css="bg-secondary" handleClick={handleClick} />
+      <Button buttonName="0" css="span-two" handleClick={handleClick} />
+      <Button buttonName="." handleClick={handleClick} />
+      <Button buttonName="=" css="bg-secondary" handleClick={handleClick} />
+    </div>
+  );
+};
 export default Calculator;
